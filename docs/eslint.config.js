4518fa42e9bd855bac15029ea6e4d46327872f6d
Jonathan Peris <jperis.silva@gmail.com>
2026-05-17 02:35:11 +0000

    Add docs favicon and lint config

---
+++ docs/eslint.config.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    ignores: [
+      'out/**',
+      'dist/**',
+      'node_modules/**',
+      '.astro/**',
+    ],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+    },
+  },
+];
